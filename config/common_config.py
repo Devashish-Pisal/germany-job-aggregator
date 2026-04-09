e8d951ab7f060862e75e9f9b66f99409f7867102
Devashish-Pisal <dpisal85@gmail.com>
2026-04-09 18:35:29 +0200

    use project to find (working student) support and QA jobs in mannheim

--- config/common_config.py
+++ config/common_config.py
@@ -3,23 +3,41 @@ config =  {
 
     # User query setting
     "country": ["germany"],                     # List of countries to search for job listings (case-insensitive)
-    "city": ["berlin", "frankfurt"],                       # List of cities to search for job listings (case-insensitive)
-    "distance": 50,                             # In kilometers
+    "city": ["mannheim"],                       # List of cities to search for job listings (case-insensitive)
+    "distance": 40,                             # In kilometers
     "remote": False,                            # If set "True", then "city", "distance" fields will be ignored completely.
     "full_time": False,
     "part_time": False,
     "search_keywords": [                        # Synonymes of search terms (at the end results for all terms will be merged and duplicated will be removed) (case-insensitive)
-        #"Werkstudent Informatik",
-        #"working student",
-        "Senior Python Developer"
+        "Werkstudent IT Support",
+        "Werkstudent IT",
+        "Werkstudent Systemadministration",
+        "Werkstudent IT Infrastruktur",
+        "Werkstudent Software Tester",
+        "Werkstudent QA",
+        "Werkstudent Technical Support",
+        "IT Support Mitarbeiter Werkstudent",
+        "IT Helpdesk Werkstudent",
+        "First Level Support Werkstudent",
+        "Second Level Support Werkstudent",
+        "IT Administrator Werkstudent",
+        "IT Operations Werkstudent",
+        "Application Support Werkstudent",
+        "Software Tester Werkstudent",
+        "QA Engineer Werkstudent",
+        "Test Engineer Werkstudent",
+        "Working Student IT Support",
+        "Working Student System Administrator",
+        "Working Student QA",
+        "Technical Support Working Student",
     ],
     "output_filename": "jobs.csv",             # Currently only .csv output file formate is supported
 
 
     # Developer setting
     "use_adzuna_api": True,
-    "use_arbeitsamt_api": False,
-    "use_arbeitsnow_api": False,
+    "use_arbeitsamt_api": True,
+    "use_arbeitsnow_api": True,
     "use_findwork_api": True,                   # Developer focussed platform
     "max_pages": 1,
     "page_number": 1,
