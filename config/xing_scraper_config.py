5d0b06bc6ac3e8ee54fe64d3b9c058be9e372fbd
Devashish-Pisal <dpisal85@gmail.com>
2026-06-27 16:43:23 +0200

    implement xing scraping config

---
+++ config/xing_scraper_config.py
@@ -0,0 +1,14 @@
+xing_scraper_config = {
+    "BASE_URL": "https://www.xing.com/jobs/search/ki?keywords={keywords}&location={location}&radius={radius}&sincePeriod={job_age}",
+    "location_radius_pairs" : {
+        # Allowed radius lengths for xing are : 0, 10, 20, 50, 70, 100, 200
+        "Mannheim": 0,
+        # "Frankfurt am Main": 20,
+    },
+    "job_age": "LAST_24_HOURS", # Allowed number of days old job for xing is : LAST_24_HOURS, LAST_WEEK, LAST_MONTH
+    "search_keywords": [
+        # Werkstudent KI,
+        # "Werkstudent Automatisierung",
+        "Werkstudent"
+    ],
+}
