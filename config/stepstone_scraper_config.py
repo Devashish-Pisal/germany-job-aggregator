e37e57866ca329c382938edc6e56d22c63015e5b
Devashish-Pisal <dpisal85@gmail.com>
2026-06-27 15:10:48 +0200

    implement stepstone scraper config

---
+++ config/stepstone_scraper_config.py
@@ -0,0 +1,12 @@
+stepstone_scraper_config = {
+    "BASE_URL": "https://www.stepstone.de/jobs/{keywords}/in-{location}?radius={radius}&action=facet_selected%3bage%3bage_1&ag=age_{job_age}&searchOrigin=Resultlist_top-search",
+    "location_radius_pairs" : {
+        # Allowed radius lengths for stepstone are : 5, 10, 20, 30, 40, 50, 75, 100
+        "Mannheim": 50,
+        #"Frankfurt am Main": 20,
+    },
+    "job_age": 1, # Allowed number of days old job for stepstone is (1, 7)
+    "search_keywords": [
+        "Werkstudent KI",
+    ],
+}
