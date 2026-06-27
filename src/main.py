dc7eb6e204f9051c64d814c09c103a44f6bc4770
Devashish-Pisal <dpisal85@gmail.com>
2026-06-27 16:45:53 +0200

    adjust imports after renaming folder

--- src/main.py
+++ src/main.py
@@ -2,11 +2,11 @@ from loguru import logger
 from path_config import LOGS_FOLDER_PATH
 from datetime import datetime
 
-from src.api_clients.arbeitsamt import Arbeitsamt
-from src.api_clients.findwork import FindWork
+from src.sources.arbeitsamt import Arbeitsamt
+from src.sources.findwork import FindWork
 from src.utils.validate_config import  ValidateConfig
 from config.common_config import config
-from src.api_clients.adzuna import Adzuna
+from src.sources.adzuna import Adzuna
 
 # TODO: CREATE DATA FOLDER AND SUBFOLDERS (IF NOT EXISTS)
 
