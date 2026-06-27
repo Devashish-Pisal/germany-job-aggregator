f0016ddb9989bc66e66dd3f8ab57e494d30756e4
Devashish-Pisal <dpisal85@gmail.com>
2026-06-27 21:29:24 +0200

    implement indeed scraper config

---
+++ config/indeed_scraper_config.py
@@ -0,0 +1,13 @@
+indeed_scraper_config = {
+    "BASE_URL": "https://de.indeed.com/jobs?q={keywords}&l={location}&fromage={job_age}&radius={radius}",
+    "location_radius_pairs": {
+        # Allowed radius lengths for stepstone are : 0, 5, 10, 15, 25, 35, 40, 50, 100
+        "Mannheim, Baden-Württemberg": 25,
+        # "Frankfurt am Main": 20,
+    },
+    "job_age": 7,  # Allowed number of days old job for stepstone is (1, 3, 7, 14)
+    "search_keywords": [
+        # "Werkstudent KI",
+        "Werkstudent Automatisierung",
+    ],
+}
