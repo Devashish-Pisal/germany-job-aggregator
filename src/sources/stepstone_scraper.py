bc436cf0747eade67ab58e703e3ba2864bccfdc6
Devashish-Pisal <dpisal85@gmail.com>
2026-06-29 14:01:32 +0200

    use dedicated class for stepstone scraper

--- src/sources/stepstone_scraper.py
+++ src/sources/stepstone_scraper.py
@@ -3,50 +3,71 @@ from playwright.sync_api import sync_playwright
 from config.stepstone_scraper_config import stepstone_scraper_config
 
 
-def build_query_urls() -> list[str]:
-    urls = []
-    location_radius_pairs = stepstone_scraper_config["location_radius_pairs"]
-    keywords_list = stepstone_scraper_config["search_keywords"]
-    job_age = stepstone_scraper_config["job_age"]
-    base_url = stepstone_scraper_config["BASE_URL"]
-    for k,v in location_radius_pairs.items():
-        for kw in keywords_list:
-            kw = kw.strip().lower().replace(" ", "-")
-            k = k.strip().lower().replace(" ", "-")
-            url = base_url.format(
-                keywords=kw,
-                location=k,
-                radius=v,
-                job_age=job_age,
+class StepstoneScraper:
+    def __init__(self):
+        self.query_urls = None
+        self.all_query_matched_job_urls = None
+        self.all_query_matched_jobs = None # scrape job descriptions and construct list of JOB objects
+        self.matching_jobs = None # Resume matching jobs in embedding space --> save these jobs into db immediately
+        self.db_saved_jobs = None # Successfully saved jobs to the DB
+
+
+    def run_scraper(self):
+        # TODO: Surround following block with if to disable scraper
+        self.query_urls = self.build_query_urls()
+        print(len(self.query_urls))
+        print(self.query_urls)
+        self.all_query_matched_job_urls = self.extract_job_urls(self.query_urls)
+        print(len(self.all_query_matched_job_urls))
+        print(self.all_query_matched_job_urls)
+
+
+    @staticmethod
+    def build_query_urls() -> list[str]:
+        urls = []
+        location_radius_pairs = stepstone_scraper_config["location_radius_pairs"]
+        keywords_list = stepstone_scraper_config["search_keywords"]
+        job_age = stepstone_scraper_config["job_age"]
+        base_url = stepstone_scraper_config["BASE_URL"]
+        for k,v in location_radius_pairs.items():
+            for kw in keywords_list:
+                kw = kw.strip().lower().replace(" ", "-")
+                k = k.strip().lower().replace(" ", "-")
+                url = base_url.format(
+                    keywords=kw,
+                    location=k,
+                    radius=v,
+                    job_age=job_age,
+                )
+                urls.append(url)
+        return urls
+
+
+    @staticmethod
+    def extract_job_urls(query_url_list:list[str]) -> set[str]:
+        job_urls = set()
+        with sync_playwright() as p:
+            browser = p.chromium.launch(
+                headless=stepstone_scraper_config["use_headless_mode"], # Headless scraping is not allowed on stepstone
             )
-            urls.append(url)
-    return urls
-
-
-def extract_job_urls(query_url_list:list[str]) -> set[str]:
-    job_urls = set()
-    with sync_playwright() as p:
-        browser = p.chromium.launch(
-            headless=stepstone_scraper_config["use_headless_mode"], # Headless scraping is not allowed on stepstone
-        )
-        page = browser.new_page()
-        for url in query_url_list:
-            print(url)
-            page.goto(url)
-            page.wait_for_selector("div[class*='res-'][data-genesis-element='BASE']")
-            valid_hits = page.locator("[data-resultlist-offers-numbers]")
-            number_of_valid_hits_displayed = int(valid_hits.get_attribute("data-resultlist-offers-main-displayed")) # only collect urls of valid jobs (not recommendations) from page 1
-            links = page.locator("a[href*='/stellenangebote']")
-            for i in range(number_of_valid_hits_displayed):
-                href = links.nth(i).get_attribute("href")
-                if href:
-                    job_urls.add("https://www.stepstone.de/"+ href)
-            page.close()
             page = browser.new_page()
-        browser.close()
-    return job_urls
+            for url in query_url_list:
+                print(url)
+                page.goto(url)
+                page.wait_for_selector("div[class*='res-'][data-genesis-element='BASE']")
+                valid_hits = page.locator("[data-resultlist-offers-numbers]")
+                number_of_valid_hits_displayed = int(valid_hits.get_attribute("data-resultlist-offers-main-displayed")) # only collect urls of valid jobs (not recommendations) from page 1
+                links = page.locator("a[href*='/stellenangebote']")
+                for i in range(number_of_valid_hits_displayed):
+                    href = links.nth(i).get_attribute("href")
+                    if href:
+                        job_urls.add("https://www.stepstone.de/"+ href)
+                page.close()
+                page = browser.new_page()
+            browser.close()
+        return job_urls
 
 
-output = extract_job_urls(build_query_urls())
-pprint(output)
-print(len(output))
+if __name__=="__main__":
+    stepstone_scraper = StepstoneScraper()
+    stepstone_scraper.run_scraper()
