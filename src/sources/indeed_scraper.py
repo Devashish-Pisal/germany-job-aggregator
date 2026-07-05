32c75320d02856c1642c39758bb4775db136fc1e
Devashish-Pisal <dpisal85@gmail.com>
2026-07-05 18:39:06 +0200

    use browser specific profile for indeed scraper

--- src/sources/indeed_scraper.py
+++ src/sources/indeed_scraper.py
@@ -1,8 +1,10 @@
 import time
 from pprint import pprint
+from loguru import logger
 from urllib.parse import quote_plus, parse_qs, urlparse
 from playwright.sync_api import sync_playwright
 from config.indeed_scraper_config import indeed_scraper_config
+from config.scraper_common_config import scraper_common_config
 
 
 class IndeedScraper:
@@ -17,19 +19,16 @@ class IndeedScraper:
     def run_scraper(self):
         # TODO: Surround following block with if to disable scraper
         self.query_urls = self.build_query_urls()
-        print(len(self.query_urls))
-        print(self.query_urls)
+        logger.info(f"[Indeed Scraper] Indeed scraper built {len(self.query_urls)} query combinations")
         self.all_query_matched_job_urls = self.extract_job_urls(self.query_urls)
-        print(len(self.all_query_matched_job_urls))
-        print(self.all_query_matched_job_urls)
-
+        logger.info(f"[Indeed Scraper] Indeed found total {len(self.all_query_matched_job_urls)} query matching job urls")
 
 
     @staticmethod
     def build_query_urls() -> list[str]:
         urls = []
         location_radius_pairs = indeed_scraper_config["location_radius_pairs"]
-        keywords_list = indeed_scraper_config["search_keywords"]
+        keywords_list = scraper_common_config["search_keywords"]
         job_age = indeed_scraper_config["job_age"]
         base_url = indeed_scraper_config["BASE_URL"]
         for k,v in location_radius_pairs.items():
@@ -49,33 +48,36 @@ class IndeedScraper:
     def extract_job_urls(self, query_url_list:list[str]) -> set[str]:
         job_urls = set()
         with sync_playwright() as p:
-            browser = p.chromium.launch(
+            browser = p.chromium.launch_persistent_context(
                 headless=indeed_scraper_config["use_headless_mode"], # Headless scraping is not allowed on xing
+                user_data_dir=scraper_common_config["scraping_browser_profile_path"], # indeed is blocking requests, browser profile might help
+                channel="chrome",
             )
             page = browser.new_page()
-            for url in query_url_list:
-                print(url)
-                page.goto(url)
-                reject_cookies = page.locator("button[id='onetrust-reject-all-handler']")
-                reject_cookies.click() # reject cookies
-                page.wait_for_selector("div[class*='jobsearch-LeftPane']")
-                empty_result = page.locator("div[class*='jobsearch-NoResult-messageContainer']").is_visible()
-                if empty_result:
-                    print(f"No results found for query {url}")
-                    continue
-                cards = page.locator("#mosaic-jobResults #mosaic-provider-jobcards ul > li[class*='css']  a[id*='job']") # > div[class*='vjs-highlight']
-                print(cards.count())
-                for i in range(cards.count()):
-                    card = cards.nth(i)
-                    href = card.get_attribute("href")
-                    if href:
-                        if href.startswith("/rc/clk?"): # only allow valid hrefs
-                            full_url = self.normalize_indeed_url("https://de.indeed.com/viewjob?"+ href[8:len(href)]) # construct full_url by removing "/rc/clk?"
-                            # Example of href scraped from HTML: "/rc/clk?jk=7d79172975914a6c&bb=r6STzscjGymbQadnjPTxYUuqozf0DHAMtElUW1i-8QubawdLKL7PNk7qHQixZLOmLO8txAQbl6aGauzPwr5RmJ9qfLu45DUHWGh48l7WOW8mIynrK5nb77Q3GAeVHwaS&xkcb=SoCm67M3hFOv3FzNV50LbzkdCdPP&fccid=6a9687b53c8c4525&cmp=eiei-4-einzelhandel-gmbh&ti=IT+Manager&vjs=3"
-                            job_urls.add(full_url)
-                page.close()
-                page = browser.new_page()
-            browser.close()
+            try:
+                for url in query_url_list:
+                    page.goto(url, wait_until="domcontentloaded")
+                    is_reject_cookies_visible = page.locator("button[id='onetrust-reject-all-handler']").is_visible()
+                    if is_reject_cookies_visible:
+                        reject_cookies = page.locator("button[id='onetrust-reject-all-handler']")
+                        reject_cookies.click() # reject cookies
+                    page.wait_for_selector("div[class*='jobsearch-LeftPane']")
+                    empty_result = page.locator("div[class*='jobsearch-NoResult-messageContainer']").is_visible()
+                    if empty_result:
+                        logger.info(f"[Indeed Scraper] No matching job posting results found for query {url}")
+                        continue
+                    cards = page.locator("#mosaic-jobResults #mosaic-provider-jobcards ul > li[class*='css']  a[id*='job']") # > div[class*='vjs-highlight']
+                    for i in range(cards.count()):
+                        card = cards.nth(i)
+                        href = card.get_attribute("href")
+                        if href:
+                            if href.startswith("/rc/clk?"): # only allow valid hrefs
+                                full_url = self.normalize_indeed_url("https://de.indeed.com/viewjob?"+ href[8:len(href)]) # construct full_url by removing "/rc/clk?"
+                                # Example of href scraped from HTML: "/rc/clk?jk=7d79172975914a6c&bb=r6STzscjGymbQadnjPTxYUuqozf0DHAMtElUW1i-8QubawdLKL7PNk7qHQixZLOmLO8txAQbl6aGauzPwr5RmJ9qfLu45DUHWGh48l7WOW8mIynrK5nb77Q3GAeVHwaS&xkcb=SoCm67M3hFOv3FzNV50LbzkdCdPP&fccid=6a9687b53c8c4525&cmp=eiei-4-einzelhandel-gmbh&ti=IT+Manager&vjs=3"
+                                job_urls.add(full_url)
+                browser.close()
+            except Exception as e:
+                logger.warning(f"[Indeed Scrapper] Caught exception {e} for url {url}")
         return job_urls
 
 
