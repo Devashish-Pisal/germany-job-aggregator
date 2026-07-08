358e5a748d2caed895cc24b4b61070319271d24c
Devashish-Pisal <dpisal85@gmail.com>
2026-07-08 16:57:56 +0200

    test scraper with embedding model

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
 from loguru import logger
+
+from config.indeed_scraper_config import indeed_scraper_config
 from path_config import LOGS_FOLDER_PATH
 from datetime import datetime
 
 from src.sources.arbeitsamt import Arbeitsamt
 from src.sources.findwork import FindWork
+from src.sources.study_smarter_scraper import StudySmarterScraper
 from src.utils.validate_config import  ValidateConfig
 from config.common_config import config
 from src.sources.adzuna import Adzuna
@@ -36,4 +39,51 @@ log_file_name = datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + "_job-aggregator-
 log_file_path = str(LOGS_FOLDER_PATH / log_file_name)
 logger.add(log_file_path, rotation="10 MB", level="INFO")
 '''
+import time
+from pprint import pprint
+from config.scraper_common_config import scraper_common_config
+from sources.indeed_scraper import IndeedScraper
+from sources.xing_scraper import XingScraper
+from sources.stepstone_scraper import StepstoneScraper
+from sources.study_smarter_scraper import StudySmarterScraper
+from sentence_transformers import SentenceTransformer
+from src.utils.util import compute_search_keywords_embeddings
 
+
+start = time.time()
+embedding_model = SentenceTransformer(scraper_common_config["embedding_match_config"]["sentence_embedding_model"])
+logger.info("Model loaded!")
+kw_embeddings = compute_search_keywords_embeddings(embedding_model, scraper_common_config["search_keywords"])
+logger.info(f"Keyword embeddings computed | First 5 are : {kw_embeddings[0:5]}")
+# indeed_result = []
+# xing_result = []
+# stepstone_result = []
+study_smarter_result = []
+
+# indeed_scraper = IndeedScraper()
+# xing_scraper = XingScraper()
+# stepstone_scraper = StepstoneScraper()
+ss_scraper = StudySmarterScraper(embedding_model=embedding_model, keyword_embeddings=kw_embeddings)
+
+# indeed_scraper.run_scraper()
+# xing_scraper.run_scraper()
+# stepstone_scraper.run_scraper()
+ss_scraper.run_scraper()
+
+# indeed_result.extend(indeed_scraper.all_query_matched_job_urls)
+# xing_result.extend(xing_scraper.all_query_matched_job_urls)
+# stepstone_result.extend(stepstone_scraper.all_query_matched_job_urls)
+study_smarter_result.extend(ss_scraper.query_matched_emb_accepted_urls)
+
+end = time.time()
+
+
+print("="*130)
+print(f"FINAL AGGREGATED RESULT - {len(study_smarter_result)} items")
+for item in study_smarter_result:
+    print(item)
+print("="*130)
+print(f"Scraping took {((end-start)/60):.2f} minutes")
+print("="*130)
+print("Rejected Items")
+pprint(ss_scraper.query_matched_emb_rejected_urls)
